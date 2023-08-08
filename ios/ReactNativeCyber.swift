aa2fd1542e4a576846ca92f098df6cabeda7f5a8
arundascts <129028779+arundascts@users.noreply.github.com>
2023-08-08 21:53:34 +0530

    Update ReactNativeCyber.swift

--- ios/ReactNativeCyber.swift
+++ ios/ReactNativeCyber.swift
@@ -10,8 +10,8 @@ import UIKit
 import flex_api_ios_sdk
 import React
 
-@objc(ReactNativeCyber)
-class ReactNativeCyber: NSObject {
+@objc(RCTReactNativeCyber)
+class RCTReactNativeCyber: NSObject {
     // ... (existing code remains the same)
 
     // Define a PaymentInfo struct to hold the payment details
