a72f3bbc36554c14f42c3b5c7c1db1049b635572
arundascts <129028779+arundascts@users.noreply.github.com>
2023-08-08 22:36:18 +0530

    Update ReactNativeCyber.m

--- ios/ReactNativeCyber.m
+++ ios/ReactNativeCyber.m
@@ -1,7 +1,7 @@
 #import <React/RCTBridgeModule.h>
 
 @interface
-RCT_EXTERN_MODULE(RCTReactNativeCyber, NSObject)
+RCT_EXTERN_MODULE(ReactNativeCyber, NSObject)
 
 RCT_EXTERN_METHOD(makeReservation: (NSString *)cardNumber  cardVerificationCode: (NSString *)cardVerificationCode cardExpirationMonth: (NSString *)cardExpirationMonth cardExpirationYear: (NSString *)cardExpirationYear callback: (RCTResponseSenderBlock)callback)
 
