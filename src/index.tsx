32b5a1546024f392f5c4e0a32b6d66d0d762e359
arundascts <129028779+arundascts@users.noreply.github.com>
2023-08-08 21:56:02 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,12 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules } from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'react_native_cyber' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+const ReactNativeCyber = NativeModules.RCTReactNativeCyber;
 
-const ReactNativeCyber = NativeModules.ReactNativeCyber
-  ? NativeModules.ReactNativeCyber
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-    export function makeReservation(ccKey: string, cardNumber: string, cardVerificationCode: string, cardExpirationMonth: string, cardExpirationYear: string, ): Promise<number> {
+    export const makeReservation = (
+      ccKey: string, 
+      cardNumber: string, 
+      cardVerificationCode: string, 
+      cardExpirationMonth: string, 
+      cardExpirationYear: string, ): Promise<number> => {
       return ReactNativeCyber.makeReservation(ccKey, cardNumber, cardVerificationCode, cardExpirationMonth, cardExpirationYear);
 }
